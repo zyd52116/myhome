57cd2d53915e29d0c7efa35ecf76ffea09c33992
hemengzhi88 <hemengzhi88@qq.com>
2015-12-30 23:14:30 +0800

    加入分页

--- config/initializers/will_paginate.rb
+++ config/initializers/will_paginate.rb
@@ -1,3 +1,3 @@
-WillPaginate::ViewHelpers.pagination_options[ :class ] = "yourclass"  
+WillPaginate::ViewHelpers.pagination_options[ :class ] = "will_paginate"  
 WillPaginate::ViewHelpers.pagination_options[:previous_label ] =  "前一页"   
 WillPaginate::ViewHelpers.pagination_options[:next_label ] =  "后一页"   
